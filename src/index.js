5293052d0b10afeead8ee9007795f70a106f42b6
Jacob Cross <jacob.m.cross1999@gmail.com>
2017-10-16 15:28:28 -0700

    Rough Draft

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import path from 'path';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import migrator from './migrator';
+
+import db from './lib/database';
+import routes from './lib/routes';
+
+let app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/v1/users', routes.v1.users);
+
+db.sequelize.authenticate().then(() => {
+  return db.sequelize.sync({ force: false });
+}).then(() => {
+  app.listen((process.env.PORT || 3000), '0.0.0.0', err => {
+    if(err) throw err;
+    console.log('Server is now running on *:', (process.env.PORT || 3000));
+  })
+}).catch(err => {
+  throw err;
+});
---
+++ src/lib/database/user.js
@@ -0,0 +1,131 @@
+const model = (sequelize, DataTypes) => {
+  let User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    gender: {
+      type: DataTypes.ENUM('male', 'female')
+    },
+    nameSet: {
+      type: DataTypes.TEXT
+    },
+    title: {
+      type: DataTypes.TEXT
+    },
+    givenName: {
+      type: DataTypes.TEXT
+    },
+    middleInitial: {
+      type: DataTypes.STRING(1)
+    },
+    surname: {
+      type: DataTypes.TEXT
+    },
+    streetAddress: {
+      type: DataTypes.TEXT
+    },
+    city: {
+      type: DataTypes.TEXT
+    },
+    state: {
+      type: DataTypes.STRING(2)
+    },
+    stateFull: {
+      type: DataTypes.TEXT
+    },
+    zipCode: {
+      type: DataTypes.TEXT
+    },
+    country: {
+      type: DataTypes.STRING(2)
+    },
+    countyFull: {
+      type: DataTypes.TEXT
+    },
+    email: {
+      type: DataTypes.TEXT
+    },
+    username: {
+      type: DataTypes.TEXT
+    },
+    password: {
+      type: DataTypes.TEXT
+    },
+    browserUserAgent: {
+      type: DataTypes.TEXT
+    },
+    telephoneNumber: {
+      type: DataTypes.TEXT
+    },
+    telephoneCountryCode: {
+      type: DataTypes.TEXT
+    },
+    mothersMaiden: {
+      type: DataTypes.TEXT
+    },
+    birthday: {
+      type: DataTypes.DATEONLY
+    },
+    age: {
+      type: DataTypes.INTEGER
+    },
+    ccType: {
+      type: DataTypes.TEXT
+    },
+    ccNumber: {
+      type: DataTypes.TEXT
+    },
+    cvv2: {
+      type: DataTypes.TEXT
+    },
+    ccExpiry: {
+      type: DataTypes.TEXT
+    },
+    nationalID: {
+      type: DataTypes.TEXT
+    },
+    ups: {
+      type: DataTypes.TEXT
+    },
+    westernUnionMTCN: {
+      type: DataTypes.TEXT
+    },
+    moneyGramMTCN: {
+      type: DataTypes.TEXT
+    },
+    color: {
+      type: DataTypes.TEXT
+    },
+    company: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    occupation: {
+      type: DataTypes.TEXT
+    },
+    vehicle: {
+      type: DataTypes.TEXT
+    },
+    domain: {
+      type: DataTypes.TEXT
+    },
+    guid: {
+      type: DataTypes.TEXT
+    },
+    latitude: {
+      type: DataTypes.REAL
+    },
+    longitude: {
+      type: DataTypes.REAL
+    }
+  });
+
+  User.associate = (models) => {
+  };
+
+  return User;
+};
+
+export default model;
